41052f5831724db8434a5ef4195581f51543ea7c
Shay Patel <shay@shay.services>
2024-01-23 23:01:55 +0000

    Create suitable UI margins

--- src/main/java/com/vaas/fullstackexercise3/Controller.java
+++ src/main/java/com/vaas/fullstackexercise3/Controller.java
@@ -22,7 +22,7 @@ public class Controller {
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
             stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setTitle("ABC");
+            stage.setTitle("Connect to a peer");
             stage.setScene(new Scene(root1));
             stage.show();
         } catch(Exception e) {
