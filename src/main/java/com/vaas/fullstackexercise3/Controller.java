7be7643955762dc34d3d21df1a08ee3778afcc58
Shay Patel <shay@shay.services>
2024-01-23 22:46:20 +0000

    Created 'Enter peer manually' menu and basic chat layout

--- src/main/java/com/vaas/fullstackexercise3/Controller.java
+++ src/main/java/com/vaas/fullstackexercise3/Controller.java
@@ -1,14 +1,33 @@
 package com.vaas.fullstackexercise3;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+//    @FXML
+//    private Label welcomeText;
+//
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void enterPeerManually() throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("add-peer.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("ABC");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            System.err.println("An error occurred opening the add-peer dialog window.");
+        }
     }
+
 }
