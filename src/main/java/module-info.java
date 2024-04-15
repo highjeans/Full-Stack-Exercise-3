d8f32b26d90aeb2943947d5517b570a8b8a1d4b2
highjeans <highjeans11@hotmail.com>
2024-04-15 12:34:38 +0100

    Add dependency requirement for getting code from post request

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.vaas.fullstackexercise3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.httpcomponents.client5.httpclient5.fluent;
+    requires org.apache.httpcomponents.core5.httpcore5;
 
 
     opens com.vaas.fullstackexercise3 to javafx.fxml;
